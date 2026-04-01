d585e87dc3b9923b679bc39b5061f4c22e7f0d1b
Fady Mohy El-Deen <fadymohy@zohomail.com>
2026-04-01 10:35:51 +0200

    refactor: craeted a layouts folder and defaultlayout.vue file with header router-view and footer and updated app.vue to use defaultlayout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AppHeader />
+    <slot />
+    <!-- any component passed to parent will be rendered here -->
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
+export default {
+  name: "DefaultLayout",
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
