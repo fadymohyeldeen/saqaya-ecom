b5fb407332733ea8c2946f15b27c0b56effc916e
Fady Mohy El-Deen <fadymohy@zohomail.com>
2026-04-18 21:37:38 +0200

    chore: upgraded to vue 3, vuex 4, vue router 4, and updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/global.css'
 
-Vue.config.productionTip = false
-Vue.config.errorHandler = function (err, _, info) {
+const app = createApp(App)
+app.config.errorHandler = function (err, _, info) {
   console.error(`[Vue Error] ${info}:`, err)
 }
 
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app')
+app.use(router)
+app.use(store)
+app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,46 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView, // eager loading
-    meta: {
-      layout: 'DefaultLayout',
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView, // eager loading
+      meta: {
+        layout: 'DefaultLayout',
+      },
     },
-  },
-  // used eager loading for home and lazy loading for others for performance
-  {
-    path: '/products',
-    name: 'products',
-    component: () => import('@/views/ProductsView.vue'), // lazy loadings
-  },
-  {
-    path: '/products/:id',
-    name: 'product',
-    component: () => import('@/views/ProductView.vue'),
-  },
-  {
-    path: '/about-us',
-    name: 'about',
-    component: () => import('@/views/AboutView.vue'),
-  },
-  {
-    path: '/contact',
-    name: 'contact',
-    component: () => import('@/views/ContactView.vue'),
-  },
-  {
-    path: '*',
-    name: 'not-found',
-    component: () => import('@/views/ErrorView.vue'),
-  },
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  // hash mode: better performance but looks ugly and not SEO friendly.
-  // history mode: looks clean and SEO friendly but requires server config (in dev vite/vue-cli handles it, config only needed in prod or it will return 404 on refresh)
-  base: import.meta.env.BASE_URL,
-  routes,
+    // used eager loading for home and lazy loading for others for performance
+    {
+      path: '/products',
+      name: 'products',
+      component: () => import('@/views/ProductsView.vue'), // lazy loadings
+    },
+    {
+      path: '/products/:id',
+      name: 'product',
+      component: () => import('@/views/ProductView.vue'),
+    },
+    {
+      path: '/about-us',
+      name: 'about',
+      component: () => import('@/views/AboutView.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/views/ContactView.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('@/views/ErrorView.vue'),
+    },
+  ],
   scrollBehavior() {
     return { x: 0, y: 0 }
   },
 })
-
 export default router
